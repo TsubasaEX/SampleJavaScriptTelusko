c247012852992cad44f187f55e21cf379ad556b6
zach.chih <zach.chih@latticeworkinc.com>
2023-06-28 17:25:46 +0800

    Add 05 06

---
+++ 05-DataType/first.js
@@ -0,0 +1,24 @@
+let data = 8    //number
+let user = "Navin"  //string
+
+let num1 = 0xff
+console.log(typeof num1);
+console.log(num1);
+
+let num2 = 1.5e12
+console.log(typeof num2);
+console.log(num2);
+
+let num3 = 100_00_000
+console.log(typeof num3);
+console.log(num3);
+
+let num4 = 5 / 0
+console.log(typeof num4);
+console.log(num4);
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MAX_VALUE * 10);
+
+let num5 = 105050505050505050505050505050501n
+console.log(num5 + 2n);
---
+++ 06-DataType2/first.js
@@ -0,0 +1,20 @@
+let firstName = "Navin"
+let lastName = "Reddy"
+
+let user = firstName + " " + lastName
+
+console.log(user);
+
+
+let v = null
+console.log(v);
+console.log(typeof v);
+
+
+let u
+console.log(u);
+console.log(typeof u);
+
+
+console.log(5 / "Navin");
+console.log(typeof (5 / "Navin"));
