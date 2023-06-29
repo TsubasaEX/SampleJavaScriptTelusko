a4347e9c5ec169edd4317d4a5072c328f9367547
zach.chih <zach.chih@latticeworkinc.com>
2023-06-29 14:32:18 +0800

    17

---
+++ 17-ForLoop/first.js
@@ -0,0 +1,14 @@
+for (let i = 1; i <= 5; i++) {
+    console.log("Hi", i);
+}
+
+
+let j = 1
+for (; j <= 5;) {
+    console.log("Hello");
+    j++;
+}
+
+// for (; ;) {
+//     console.log("Hello");
+// }
