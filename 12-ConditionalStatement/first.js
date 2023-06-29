ab5677f84e4dda5d9e3035bbba2b940694041f88
zach.chih <zach.chih@latticeworkinc.com>
2023-06-29 14:16:29 +0800

    12

---
+++ 12-ConditionalStatement/first.js
@@ -0,0 +1,13 @@
+
+let num1 = 6
+let num2 = 4
+let num3 = 7
+
+
+if (num1 > num2 && num1 > num3) {
+    console.log("num1 is greater");
+} else if (num2 > num1 && nun2 > num3) {
+    console.log("num2 is greater");
+} else {
+    console.log("num3 is greatest");
+}
