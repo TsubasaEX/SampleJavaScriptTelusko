e128404068431ee859c64d265cb81acf8b9a66a4
zach.chih <zach.chih@latticeworkinc.com>
2023-06-29 14:38:06 +0800

    19

---
+++ 19-Object/first.js
@@ -0,0 +1,14 @@
+let input = 'name'
+
+let alien = {
+    name: 'Navin',
+    tech: 'JS',
+    'work exp': 4
+}
+
+
+console.log(alien.name);
+console.log(alien['tech']);
+console.log(alien['work exp']);
+
+console.log(alien[input]);
