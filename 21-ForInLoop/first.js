7c4b8d59cb03aef1df9780f887462072a69f263c
zach.chih <zach.chih@latticeworkinc.com>
2023-06-29 14:49:28 +0800

    21

---
+++ 21-ForInLoop/first.js
@@ -0,0 +1,14 @@
+
+let alien = {
+    name: 'Navin',
+    tech: 'JS',
+    laptop: {
+        cpu: 'i7',
+        ram: 4,
+        brand: 'Asus'
+    }
+}
+
+for (let key in alien) {
+    console.log(key, alien[key]);
+}
