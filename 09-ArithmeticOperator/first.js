c03337fbd23b4cfcde66f9bede3043673c3a014d
zach.chih <zach.chih@latticeworkinc.com>
2023-06-29 14:04:06 +0800

    Add 09

---
+++ 09-ArithmeticOperator/first.js
@@ -0,0 +1,17 @@
+let num1 = true
+let num2 = true
+
+let result = num1 + num2
+console.log(result);
+
+let num = 4
+let x = num++
+let y = ++num
+console.log(x);
+console.log(y);
+
+let a = Math.pow(4, 3)
+console.log(a);
+
+let b = 4 ** 3
+console.log(b);
