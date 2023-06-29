a248c11bec194886ce3bb61663faf8d6fe3e6a6d
zach.chih <zach.chih@latticeworkinc.com>
2023-06-29 17:54:48 +0800

    40

---
+++ 40-ImportExport/first.js
@@ -0,0 +1,4 @@
+import { captalizeString } from "./string_function.js";
+const cap = captalizeString("hello!");
+
+console.log(cap);
---
+++ 40-ImportExport/string_function.js
@@ -0,0 +1,1 @@
+export const captalizeString = str => str.toUpperCase()
